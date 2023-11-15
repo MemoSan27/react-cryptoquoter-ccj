cce3ece5aa7021ccb530a5fffde4d55297cc4ee0
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-11-15 16:55:53 -0700

    submit button created

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled"
+
+const InputSubmit = styled.input`
+    background-color: #9497ff;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #fff;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color 300ms ease;
+
+    &:hover{
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+`;
+
+const Formulario = () => {
+  return (
+    <form>
+     <InputSubmit 
+        type="submit" 
+        value="Cotizar" 
+      />
+    </form>
+  )
+}
+
+export default Formulario
