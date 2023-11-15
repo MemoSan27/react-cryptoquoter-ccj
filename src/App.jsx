d3cdf834e1d7e87142b1c75ee5d08bfd8dde0cb9
Guillermo Sandoval <tinta.mas.toner.mzt@gmail.com>
2023-11-15 16:33:53 -0700

    Initializing project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import styled from "@emotion/styled"
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+`
+
+function App() {
+ 
+
+  return (
+    <div>
+      <Heading> Desde App </Heading>
+    </div>
+  )
+}
+
+export default App
